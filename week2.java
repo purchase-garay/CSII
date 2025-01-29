b45b6614ae5841b32c066406c393d4fa78c09c9d
purchase-garay <paul.garay1229@gmail.com>
2025-01-29 16:29:21 -0500

    Add files via upload
    Created time program

---
+++ week2.java
@@ -0,0 +1,14 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class week2 {
+    LocalTime time = LocalTime.now();
+    String hour = time.format(DateTimeFormatter.ofPattern("HH"));
+    String minute = time.format(DateTimeFormatter.ofPattern("mm"));
+    String newTime = LocalTime.now().plusMinutes(55).format(DateTimeFormatter.ofPattern("HH:mm"));
+
+    public void main(String[] args) throws Exception {
+        System.out.println("The current time is" + " " + hour + ":" + minute +".");
+        System.out.println("In 55 minutes, it will be " + newTime);
+    }
+}
